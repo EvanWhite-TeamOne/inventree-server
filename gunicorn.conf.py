5a29b54680fcf7653bbb0ba7d2d6b8dc84052adc
evan <evan@teamonenetworking.com>
2025-05-02 11:10:48 -0700

    basic config changes saved for testing

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,12 +2,16 @@
 
 import multiprocessing
 
-bind = '0.0.0.0:8000'
+bind = '127.0.0.1:8000'
 
 workers = multiprocessing.cpu_count() * 2 + 1
 
 max_requests = 1000
 max_requests_jitter = 50
+
+#accesslog = "/var/log/inventree/gunicorn_access.log"
+#errorlog = "/var/log/inventree/gunicorn_error.log"
+#loglevel = "info"
 
 # preload app so that the ready functions are only executed once
 preload_app = True
